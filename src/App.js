e02dc749224e61181b9e1cabc6b1259efc95be14
optimistiks <mail@potapovmax.com>
2017-06-06 15:35:18 +0300

    Responsiveness

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ import IconButton from 'material-ui/IconButton';
 import { List, ListItem, makeSelectable } from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import MediaQuery from 'react-responsive'
 
 const SelectableList = makeSelectable(List)
 
@@ -31,7 +35,7 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      isSidebarOpened: true
+      isSidebarOpened: false
     }
     this.toggleSidebar = this.toggleSidebar.bind(this)
   }
@@ -43,118 +47,153 @@ class App extends Component {
   }
 
   render () {
-    const card = (
-      <div className='col-sm-6 col-md-4'>
-        <Card style={{ marginBottom: '30px' }}>
-          <CardTitle title="Card title" subtitle="Card subtitle"/>
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-            <FlatButton label="Action1"/>
-            <FlatButton label="Action2"/>
-          </CardActions>
-        </Card>
-      </div>
-    )
+
 
     console.log('muiTheme', this.context.muiTheme)
     const { appBar, drawer, spacing, zIndex } = this.context.muiTheme
-
+    const { isSidebarOpened } = this.state
     return (
       <div>
-        <NavBar toggleSidebar={this.toggleSidebar} />
-        <Sidebar isOpened={this.state.isSidebarOpened} />
-        <div className='flex-container'>
-          <div style={{ marginLeft: `${this.state.isSidebarOpened ? drawer.width + spacing.desktopGutterLess : 0}px`, transition: `${transitions.easeOut()}` }}>
-            <div className='container-fluid'>
-              <div className='row'>
-                <div className='col-xs-12'>
-                  <h2>Documents</h2>
-                  <div className='row'>
-                    {card}
-                    {card}
-                    {card}
-                    {card}
-                    {card}
-                    {card}
-                    {card}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <MediaQuery maxWidth={767}>
+          <NavBar toggleSidebar={this.toggleSidebar} ControlsComponent={NavBarControlsSmall} />
+          <Sidebar isOpened={isSidebarOpened} isDocked={false} />
+          <ContentWrapper marginLeft={0} />
+        </MediaQuery>
+        <MediaQuery minWidth={768} maxWidth={1199}>
+          <NavBar toggleSidebar={this.toggleSidebar} ControlsComponent={NavBarControlsLarge} />
+          <Sidebar isOpened={isSidebarOpened} isDocked={false} />
+          <ContentWrapper marginLeft={0} />
+        </MediaQuery>
+        <MediaQuery minWidth={1200}>
+          <NavBar ControlsComponent={NavBarControlsLarge} />
+          <Sidebar isOpened={true} isDocked />
+          <ContentWrapper marginLeft={drawer.width + spacing.desktopGutterLess} />
+        </MediaQuery>
       </div>
     );
   }
 }
 
-export default App;
+const muiTheme = getMuiTheme({
+  zIndex: {
+    appBar: 1300,
+    drawer: 1200,
+    drawerOverlay: 1100
+  },
+});
+
+function MuiThemeApp (props) {
+  return (
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <App {...props} />
+    </MuiThemeProvider>
+  )
+}
+export default MuiThemeApp;
+
+/* NavBar */
 
 NavBar.contextTypes = {
   muiTheme: PropTypes.object.isRequired
 }
 
 NavBar.propTypes = {
-  toggleSidebar: PropTypes.func.isRequired
+  toggleSidebar: PropTypes.func,
+  ControlsComponent: PropTypes.func.isRequired
 }
 
 function NavBar (props, context) {
-  const { toggleSidebar } = props
+  const { toggleSidebar, ControlsComponent } = props
   const { appBar } = context.muiTheme
   return (
     <AppBar
-      zDepth={2}
-      onLeftIconButtonTouchTap={toggleSidebar}
       title='Writing Tool'
-      showMenuIconButton
+      zDepth={2}
+      style={{ position: 'fixed', top: '0px' }}
+      showMenuIconButton={toggleSidebar != null}
+      onLeftIconButtonTouchTap={toggleSidebar}
       iconStyleRight={{ marginTop: '0px' }}
-      iconElementRight={
-        <ToolbarGroup
-          style={{ height: `${appBar.height}px` }}
-        >
-          <RaisedButton label="New Document"/>
-          <IconButton><AssIcon color={appBar.textColor}/></IconButton>
-          <IconButton><AccBalIcon color={appBar.textColor}/></IconButton>
-          <IconButton><AccCircleIcon color={appBar.textColor}/></IconButton>
-          <IconButton><AspectRatioIcon color={appBar.textColor}/></IconButton>
-          <ToolbarSeparator />
-          <IconMenu
-            iconButtonElement={
-              <IconButton><MoreVertIcon color={appBar.textColor}/></IconButton>
-            }
-            targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-            anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-          >
-            <MenuItem primaryText="Sign out"/>
-          </IconMenu>
-        </ToolbarGroup>
-      }
+      iconElementRight={<ControlsComponent />}
     />
   )
 }
 
+/* NavBarControlsSmall */
+
+NavBarControlsSmall.contextTypes = {
+  muiTheme: PropTypes.object.isRequired
+}
+
+function NavBarControlsSmall (props, context) {
+  const { appBar } = context.muiTheme
+  return (
+    <ToolbarGroup style={{ height: `${appBar.height}px` }}>
+      <IconButton><AspectRatioIcon color={appBar.textColor}/></IconButton>
+      <IconMenu
+        iconButtonElement={
+          <IconButton><MoreVertIcon color={appBar.textColor}/></IconButton>
+        }
+        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+      >
+        <MenuItem primaryText="New document"/>
+        <MenuItem primaryText="Writer profile"/>
+        <MenuItem primaryText="Settings"/>
+        <MenuItem primaryText="Help"/>
+        <MenuItem primaryText="Sign out"/>
+      </IconMenu>
+    </ToolbarGroup>
+  )
+}
+
+/* NavBarControlsLarge */
+
+NavBarControlsLarge.contextTypes = {
+  muiTheme: PropTypes.object.isRequired
+}
+
+function NavBarControlsLarge (props, context) {
+  const { appBar } = context.muiTheme
+  return (
+    <ToolbarGroup style={{ height: `${appBar.height}px` }}>
+      <RaisedButton label="New Document"/>
+      <IconButton><AssIcon color={appBar.textColor}/></IconButton>
+      <IconButton><AccBalIcon color={appBar.textColor}/></IconButton>
+      <IconButton><AccCircleIcon color={appBar.textColor}/></IconButton>
+      <IconButton><AspectRatioIcon color={appBar.textColor}/></IconButton>
+      <ToolbarSeparator />
+      <IconMenu
+        iconButtonElement={
+          <IconButton><MoreVertIcon color={appBar.textColor}/></IconButton>
+        }
+        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+      >
+        <MenuItem primaryText="Sign out"/>
+      </IconMenu>
+    </ToolbarGroup>
+  )
+}
+
+/* Sidebar */
+
+Sidebar.propTypes = {
+  isOpened: PropTypes.bool.isRequired,
+  isDocked: PropTypes.bool.isRequired
+}
 
 Sidebar.contextTypes = {
   muiTheme: PropTypes.object.isRequired
 }
 
-Sidebar.propTypes = {
-  isOpened: PropTypes.bool.isRequired
-}
-
 function Sidebar (props, context) {
-  const { isOpened } = props
-  const { appBar, zIndex } = context.muiTheme
+  const { appBar } = context.muiTheme
   return (
     <Drawer
-      open={isOpened}
-      zDepth={1}
-      containerStyle={{ top: `${appBar.height}px`, zIndex: zIndex.menu }}
+      open={props.isOpened}
+      docked={props.isDocked}
+      containerStyle={{ top: `${appBar.height}px` }}
+      zDepth={2}
     >
       <SelectableList value={3}>
         <ListItem
@@ -179,5 +218,67 @@ function Sidebar (props, context) {
         />
       </SelectableList>
     </Drawer>
+  )
+}
+
+/* Content */
+
+function Content () {
+  const card = (
+    <div className='col-sm-6 col-md-4'>
+      <Card style={{ marginBottom: '30px' }}>
+        <CardTitle title="Card title" subtitle="Card subtitle"/>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions>
+          <FlatButton label="Action1"/>
+          <FlatButton label="Action2"/>
+        </CardActions>
+      </Card>
+    </div>
+  )
+
+  return (
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-xs-12'>
+          <h2>Documents</h2>
+          <div className='row'>
+            {card}
+            {card}
+            {card}
+            {card}
+            {card}
+            {card}
+            {card}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+/* ContentWrapper */
+
+ContentWrapper.propTypes = {
+  marginLeft: PropTypes.number.isRequired
+}
+
+ContentWrapper.contextTypes = {
+  muiTheme: PropTypes.object.isRequired
+}
+
+function ContentWrapper (props, context) {
+  const { marginLeft } = props
+  const { appBar } = context.muiTheme
+  return (
+    <div style={{ marginTop: `${appBar.height}px`, marginLeft: `${marginLeft}px` }}>
+      <Content />
+    </div>
+
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './bootstrap.css';
@@ -10,11 +9,7 @@ import './index.css';
 injectTapEventPlugin()
 
 ReactDOM.render(
-    (
-        <MuiThemeProvider>
-            <App />
-        </MuiThemeProvider>
-    ),
+    <App />,
     document.getElementById('root')
 );
 
